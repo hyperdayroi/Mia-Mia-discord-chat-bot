7532c4c4ff404fda584e3fda4ad4aae0959bcb3e
Hyper-Duycubu <116535247+hyperdayroi@users.noreply.github.com>
2026-07-25 14:53:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
-// Entry point giữ để tương thích ngược (một số nơi có thể gọi `node index.js`).
-// Logic thật nằm ở src/index.js.
-import "./src/index.js";
+import { ActivityType } from "discord.js";
+import persona from "./personas/index.js";
+import {
+  DISCORD_TOKEN,
+  AUTO_CHAT_ENABLED,
+  AUTO_CHAT_INTERVAL,
+  FAMILY_CHAT_MODE,
+  GOOD_MORNING_ENABLED,
+  GOOD_NIGHT_ENABLED,
+  TATTLE_ENABLED
+} from "./config/env.js";
+import { createDiscordClient } from "./discord/client.js";
+import { registerCommands } from "./discord/registerCommands.js";
+import { registerInteractionHandlers } from "./discord/interactions.js";
+import { registerMessageHandlers } from "./discord/messages.js";
+import { startInternalServer } from "./family/server.js";
+import { initConversationManager, scheduleAutonomousConversation } from "./family/conversationManager.js";
+import { scheduleGreetings } from "./family/greetings.js";
+import { scheduleTattle } from "./family/tattle.js";
+
+const client = createDiscordClient();
+
+await registerCommands();
+
+client.once("ready", () => {
+  console.log(persona.texts.onlineLog(client.user.tag));
+  client.user.setPresence({
+    activities: [{ name: persona.presence.name, type: ActivityType.Playing }],
+    status: "online"
+  });
+});
+
+registerInteractionHandlers(client);
+registerMessageHandlers(client);
+
+initConversationManager(client);
+startInternalServer();
+
+if (AUTO_CHAT_ENABLED) {
+  console.log(
+    `Auto family-chat bật (mỗi ${AUTO_CHAT_INTERVAL}ms, chế độ ${FAMILY_CHAT_MODE.toUpperCase()}).`
+  );
+  scheduleAutonomousConversation(AUTO_CHAT_INTERVAL);
+}
+
+if (GOOD_MORNING_ENABLED || GOOD_NIGHT_ENABLED) {
+  console.log("Good morning/night greeting bật.");
+  scheduleGreetings();
+}
+
+if (TATTLE_ENABLED) {
+  console.log("Random tattle (mách lẻo) bật.");
+  scheduleTattle();
+}
+
+process.on("unhandledRejection", err => console.error("UNHANDLED_REJECTION:", err));
+process.on("uncaughtException", err => console.error("UNCAUGHT_EXCEPTION:", err));
+
+client.login(DISCORD_TOKEN);
