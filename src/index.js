ed7a9c160b5c113f7a197b9c2fd2ae83d5df3520
Hyper-Duycubu <116535247+hyperdayroi@users.noreply.github.com>
2026-08-07 16:28:26 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+import ffmpegPath from "ffmpeg-static";
+process.env.FFMPEG_PATH = ffmpegPath;
+
 import { ActivityType } from "discord.js";
 import persona from "./personas/index.js";
-import { DISCORD_TOKEN, AUTO_CHAT_ENABLED, AUTO_CHAT_INTERVAL, FAMILY_CHAT_MODE } from "./config/env.js";
+import {
+  DISCORD_TOKEN,
+  AUTO_CHAT_ENABLED,
+  AUTO_CHAT_INTERVAL,
+  FAMILY_CHAT_MODE,
+  GOOD_MORNING_ENABLED,
+  GOOD_NIGHT_ENABLED,
+  TATTLE_ENABLED
+} from "./config/env.js";
 import { createDiscordClient } from "./discord/client.js";
 import { registerCommands } from "./discord/registerCommands.js";
 import { registerInteractionHandlers } from "./discord/interactions.js";
 import { registerMessageHandlers } from "./discord/messages.js";
 import { startInternalServer } from "./family/server.js";
 import { initConversationManager, scheduleAutonomousConversation } from "./family/conversationManager.js";
+import { scheduleGreetings } from "./family/greetings.js";
+import { scheduleTattle } from "./family/tattle.js";
 
 const client = createDiscordClient();
 
@@ -31,6 +44,16 @@ if (AUTO_CHAT_ENABLED) {
     `Auto family-chat bật (mỗi ${AUTO_CHAT_INTERVAL}ms, chế độ ${FAMILY_CHAT_MODE.toUpperCase()}).`
   );
   scheduleAutonomousConversation(AUTO_CHAT_INTERVAL);
+}
+
+if (GOOD_MORNING_ENABLED || GOOD_NIGHT_ENABLED) {
+  console.log("Good morning/night greeting bật.");
+  scheduleGreetings();
+}
+
+if (TATTLE_ENABLED) {
+  console.log("Random tattle (mách lẻo) bật.");
+  scheduleTattle();
 }
 
 process.on("unhandledRejection", err => console.error("UNHANDLED_REJECTION:", err));
